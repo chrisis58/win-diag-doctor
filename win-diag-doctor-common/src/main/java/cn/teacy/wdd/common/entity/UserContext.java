1214b5dd5461dbf3235d82c84fa452e6909eaad6
chrisis <chrisis58@foxmail.com>
2025-12-12 19:51:26 +0800

    fix: 使用包装类以支持 getter 和 setter

--- win-diag-doctor-common/src/main/java/cn/teacy/wdd/common/entity/UserContext.java
+++ win-diag-doctor-common/src/main/java/cn/teacy/wdd/common/entity/UserContext.java
@@ -8,8 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserContext {
-    private boolean isAdmin;
-    private boolean isReader;
+    private Boolean isAdmin;
+    private Boolean isReader;
 
     public static UserContext UNKNOWN = new UserContext(false, false);
 }
