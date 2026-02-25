b07b6c3a45d7d83d490eac5f3f49ff2efd67fdc1
chrisis <chrisis58@outlook.com>
2026-02-25 22:19:54 +0800

    fix: 在迭代结束后清除计数器

--- win-diag-doctor-app/src/main/java/cn/teacy/wdd/agent/graph/dispatcher/LoopAnalystDispatcher.java
+++ win-diag-doctor-app/src/main/java/cn/teacy/wdd/agent/graph/dispatcher/LoopAnalystDispatcher.java
@@ -2,26 +2,41 @@ package cn.teacy.wdd.agent.graph.dispatcher;
 
 import cn.teacy.wdd.agent.graph.LogAnalyseGraphComposer;
 import com.alibaba.cloud.ai.graph.OverAllState;
-import com.alibaba.cloud.ai.graph.action.EdgeAction;
+import com.alibaba.cloud.ai.graph.RunnableConfig;
+import com.alibaba.cloud.ai.graph.action.Command;
+import com.alibaba.cloud.ai.graph.action.CommandAction;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Component
-public class LoopAnalystDispatcher implements EdgeAction {
+public class LoopAnalystDispatcher implements CommandAction {
 
     private static final String KEY_EXECUTOR_INSTRUCTION = LogAnalyseGraphComposer.KEY_EXECUTOR_INSTRUCTION;
     private static final String KEY_ITERATION_COUNT = LogAnalyseGraphComposer.KEY_ITERATION_COUNT;
 
     @Override
-    public String apply(OverAllState state) {
+    public Command apply(OverAllState state, RunnableConfig config) {
+
+        if (shouldLoop(state)) {
+            return new Command("loop");
+        } else {
+            return new Command(
+                    "end",
+                    Map.of(KEY_ITERATION_COUNT, 0)
+            );
+        }
+    }
+
+    private boolean shouldLoop(OverAllState state) {
         Optional<Object> instruction = state.value(KEY_EXECUTOR_INSTRUCTION);
 
         int count = (int) state.value(KEY_ITERATION_COUNT).orElseThrow();
         if (count >= 3) {
-            return "end";
+            return false;
         }
 
-        return instruction.isPresent() && !instruction.get().toString().isBlank() ? "loop" : "end";
+        return instruction.isPresent() && !instruction.get().toString().isBlank();
     }
 }
